8f8a2f115d274c2354c4ecd651d877395e0298e3
ZioPao <picchetti1998@gmail.com>
2024-05-16 12:16:13 +0200

    fixes for steam BB code

--- markdown_to_bb.py
+++ markdown_to_bb.py
@@ -15,11 +15,12 @@ def markdown_to_bbcode(markdown_text):
         bold_regex: r'[b]\1[/b]',
         italic_regex: r'[i]\1[/i]',
         code_regex: r'[code]\1[/code]',
-        header_regex: lambda match: f"[center][color=#f1c232][size={7 - len(match.group(1))}][b]{match.group(2)}[/b][/size][/color][/center]",
+        header_regex: lambda match: f"[h1]{match.group(2)}[/h1]",
         unordered_list_regex: r'[*]\1',
         ordered_list_regex: lambda match: f"[#]{match.group(1)}",
         link_regex: r'[url=\2]\1[/url]',
     }
+    # [url=https://ko-fi.com/M4M7IERNW][img]https://storage.ko-fi.com/cdn/kofi3.png[/img][/url]
 
     # Applying the regular expressions for translation
     bbcode_text = markdown_text
@@ -32,30 +33,46 @@ def markdown_to_bbcode(markdown_text):
     return bbcode_text
 
 import os
-
+import sys
 if __name__ == "__main__":
-    # iterate over .md files in the in directory
-    for filename in os.listdir("in"):
-        if filename.endswith(".md"):
-            with open(os.path.join("in", filename), "r") as f:
-                markdown_text = f.read()
-                bbcode_text = markdown_to_bbcode(markdown_text)
-                bbcode_lines = bbcode_text.split("\n")
-                
-                is_list = False
-                for i in range(len(bbcode_lines)):
-                    # match line with images
-                    img_pattern = r'\!\[url=([^\]]+\.(?:gif|png|jpg))\]([^/]+)\[/url\]'
-                    if re.match(img_pattern, bbcode_lines[i]):
-                        image_url = re.search(img_pattern, bbcode_lines[i]).group(1)
-                        print(image_url)
-                        # change to [img]https://raw.githubusercontent.com/D7ry/wheeler/main/images/item_usage.gif[/img]
-                        modified_img_string = f"[center][img]https://raw.githubusercontent.com/D7ry/wheeler/main/{image_url}[/img][/center]"
-                        bbcode_lines[i] = modified_img_string
-                        if i < len(bbcode_lines) - 1: # this assumes that for every image, we have a italicized caption one line below
-                            bbcode_lines[i+1] = f"[center]{bbcode_lines[i+1]}[/center]"
 
-                # write to .bb file in the out directory
-                bbcode_text = "\n".join(bbcode_lines)
-                with open(os.path.join("out", filename[:-3] + ".bb"), "w") as out_file:
-                    out_file.write(bbcode_text)
+    try:
+        user = sys.argv[1]
+        workspace = sys.argv[2]      # ${workspaceFolderBasename}
+    except IndexError:
+        print("You didn't pass the correct parameter.")
+        exit()
+
+    zomboid_folder = "C:\\Users\\" + user + "\\Zomboid"
+
+    mod_folder = os.path.join(zomboid_folder, "mods", workspace)
+    input = os.path.join(mod_folder, "README.md")
+
+    # get main readme.MD
+    with open(input, "r") as f:
+        markdown_text = f.read()
+        bbcode_text = markdown_to_bbcode(markdown_text)
+        bbcode_lines = bbcode_text.split("\n")
+        
+        is_list = False
+        for i in range(len(bbcode_lines)):
+            # match line with images
+            # [url=https://ko-fi.com/M4M7IERNW][img]https://storage.ko-fi.com/cdn/kofi3.png[/img][/url]
+            img_pattern = r'\[url=([^\]]+\.(?:gif|png|jpg|svg))\]([^/]+)\[/url\]'
+            if re.match(img_pattern, bbcode_lines[i]):
+                image_url = re.search(img_pattern, bbcode_lines[i]).group(1)
+                print(image_url)
+
+                url_pattern = r'\[url=.*?\]\(([^)]+)\)'
+                if re.match(url_pattern, bbcode_lines[i]):
+                    actual_url = re.search(url_pattern, bbcode_lines[i]).group(1)
+                    print(actual_url)
+                    modified_img_string = f"[url={actual_url}][img]{image_url}[/img][/url]"
+                    bbcode_lines[i] = modified_img_string
+
+        # write to .bb file in the out directory
+        bbcode_text = "\n".join(bbcode_lines)
+
+        output_path = os.path.join(mod_folder, "workshop_files", "workshop_desc.bb")
+        with open(output_path, "w") as out_file:
+            out_file.write(bbcode_text)
